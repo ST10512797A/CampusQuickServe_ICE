edaad9398a2e5560a7ac18dcac1733be9bced033
Adam Borgström <164683968+B0rgy@users.noreply.github.com>
2026-03-04 23:01:42 +0200

    Fixed constructor and calculation logic

--- CampusQuickServe_ICE/src/campusquickserve_ice/Order.java
+++ CampusQuickServe_ICE/src/campusquickserve_ice/Order.java
@@ -2,6 +2,9 @@ package campusquickserve_ice;
 
 public class Order {
 
+    //Constant
+    public static final double VAT_RATE = 0.15;
+    
         //Attributes for calculations
         String customerName;
         String = studentNumber;
@@ -10,14 +13,29 @@ public class Order {
         double = pricePerItem;        
 
         
-        //Constructor
-        public Order(String customerName, String studentNumber, String itemName, int quantity, double pricePerItem) {
+    public Order(String customerName, String studentNumber, String itemName,
+                 int quantity, double pricePerItem) {
 
-            this.customerName = customerName;
-            this.studentNumber = studentNumber;
-            this.itemName = itemName;
-            this.quantity = quantity;
-            this.pricePerItem = pricePerItem;        
-        }
-
+        this.customerName = customerName;
+        this.studentNumber = studentNumber;
+        this.itemName = itemName;
+        this.quantity = quantity;
+        this.pricePerItem = pricePerItem;
+    }
+        
+    
+    public double calculateSubtotal() {
+      return quantity * pricePerItem;  
+    };
+    
+    public double calculateVAT() {
+        double afterDiscount = calculateSubtotal() - calculateDiscount();
+        returm afterDiscount = VAT_RATE;
+    }
+    
+    public double calculateFinalTotal() {
+        returm (calculateSubtotal() - )
+    }
 }
+
+
