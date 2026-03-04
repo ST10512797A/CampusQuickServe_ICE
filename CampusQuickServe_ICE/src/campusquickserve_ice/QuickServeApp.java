bb8b7bda00ce341da6e3bfcb5ef334e937d0d732
Adam Borgström <164683968+B0rgy@users.noreply.github.com>
2026-03-04 18:04:08 +0200

    Added GUI Input with JOptionPane
    Added inputs for: Customer name, Student number, Item, Quantity, and Price

--- CampusQuickServe_ICE/src/campusquickserve_ice/QuickServeApp.java
+++ CampusQuickServe_ICE/src/campusquickserve_ice/QuickServeApp.java
@@ -4,10 +4,23 @@
  */
 package campusquickserve_ice;
 
-/**
- *
- * @author Adam
- */
+import javax.swing.JOptionPane;
+
 public class QuickServeApp {
+
+public static void man(String[] args) {
+
+    //JOptionPane Input
+    String name = JOptionPane.showInputDialog("Enter Customer Name:");
+    String StudentNo = JOptionPane.showInputDialog("Enter Student Number:");
+    String Item = JOptionPane.showInputDialog("Enter Item Ordered:");
+    int quantity = Integer.parseInt(
+            JOptionPane.showInputDialog("Enter Quantity")
+    );
+    double price = Double.parseDouble(
+            JOptionPane.showInputDialog("Enter Price per Item")
+    );
+    
+}
     
 }
